aa332ee983bc4249cc1c94b1c18694418e201b09
Paulo Loyo <pauloloyo@icloud.com>
2024-04-14 21:31:49 -0500

    More css done and some Javascript for Menu button and adding effect to navbar when scroll

---
+++ main.jsx
@@ -0,0 +1,23 @@
+const nav = document.querySelector("nav");
+const menuIcon = document.querySelector('.menuIcon');
+const menuContainer = document.querySelector(".menuContainer");
+const cartIcon = document.querySelector("cartIcon");
+const cart = document.querySelector(".cart");
+const closeIcon = document.querySelector(".menuContainer .closeIcon");
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 30) {
+        nav.classList.add("scrolled");
+    } else{
+        nav.classList.remove("scrolled");
+    }
+} );
+
+menuIcon.addEventListener("click", () =>{
+    menuContainer.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () =>{
+    menuContainer.classList.remove("active");
+});
