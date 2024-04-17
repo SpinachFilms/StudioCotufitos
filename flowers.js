490580c2b7b3af2fec47fd3e4171847d8efc69b3
Paulo Loyo <pauloloyo@icloud.com>
2024-04-17 18:52:02 -0500

    Cart implemented on HTML and CSS, working on Java

--- flowers.js
+++ flowers.js
@@ -1,30 +1,9 @@
-function App() {
-    const [selectedProductId, setSelectedProductId] = useState(null);
-    const selectedProduct = Inventory.find((product) => product.id === selectedProductId);
-  
-    function handleClick(product) {
-      console.log("Product name: ", product.name);
-      setSelectedProductId(product.id);
-    }
-    
-    return (
-      <div className='App'>
-        {
-          Inventory.map((product) => {
-            return <p onClick={() => handleClick(product)} key={product.id}>{product.name} {product.price}</p>;
-          })
-        }
-        {selectedProduct && 
-          <div>
-            <h2>{selectedProduct.name}</h2>
-            <ul>
-              <li>Name: {selectedProduct.name}</li>
-              <li>Price: {selectedProduct.price}</li>
-            </ul>
-          </div>
-        }
-      </div>
-    );
-  }
-  
-  export default App;
+const btnCart = document.querySelector ('.cartVector')
+const constainerCartProducts = document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', () => {
+  constainerCartProducts.classList.toggle('hidden-cart')
+});
+
+
+
--- main.jsx
+++ main.jsx
@@ -5,6 +5,10 @@ const cartIcon = document.querySelector("cartIcon");
 const cart = document.querySelector(".cart");
 const closeIcon = document.querySelector(".menuContainer .closeIcon");
 
+import * as React from 'react';
+declare const ShoppingBagIcon: React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & { title?: string, titleId?: string } & React.RefAttributes<SVGSVGElement>>;
+export default ShoppingBagIcon;
+
 window.addEventListener("scroll", () => {
     if (window.scrollY > 30) {
         nav.classList.add("scrolled");
